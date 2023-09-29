bfb8e40ae6e2ed5bd1e56f8057cd1dcec9f8a4b2
shrimantnavmathkasode <142007633+shrimantnavmathkasode@users.noreply.github.com>
2023-09-29 16:50:41 +0530

    Create README.cpp

---
+++ README.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+  cout<<"enter the first number";
+  cin>>a;
+  cout<<"enter the second number";
+  cin>>b;
+  int result;
+  cout<<"this is your sum"<<result<<endl;
+  
+
+
+
+return 0;
+}
